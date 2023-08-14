c84698691037a9ea8f09fb6a50a729ffdc678974
TamaraFerr <tamarablacksmit@hotmail.com>
2023-08-14 02:45:48 -0300

    FEAT: POST/sign-up and POST/Tweets

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import express from "express"
 import cors from "cors"
 
+//configs
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+//consts para guardar informações
+const tweets = [];
+const usuarios = [];
+
+//endpoints
 app.post("/sign-up" , (req, res) => {
-   
+    const {username, avatar} = req.body
+
+    usuarios.push({username, avatar})
+    res.send("OK")
 })
 
 app.post("/tweets" , (req, res) => {
-   
+    const { username, tweet} = req.body
+
+    const ExisteUsuario = usuarios.find((usuario) => usuario.username === username)
+
+    if(!ExisteUsuario){
+        return res.send("UNAUTHORIZED")
+    }
+
+    tweets.push({username, tweet})
+    res.send("OK")
 })
 
 app.get("/tweets" , (req, res) => {
